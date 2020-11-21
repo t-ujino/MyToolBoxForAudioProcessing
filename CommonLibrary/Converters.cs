cf3916cc31f36658d94c309cc7ad2341523700f1
Tadashi Ujino <t.ujino.priv+github@gmail.com>
2020-11-21 21:25:50 +0900

    :zap: Add Float to IEEE754 converter

---
+++ CommonLibrary/Converters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MyToolBoxCommonLibrary
+{
+    public enum ENDIAN
+    {
+        LittleEndian,
+        BigEndian
+    }
+
+    public class Converters
+    {
+        public static IEnumerable<byte> ToIeee754(float value, ENDIAN toEndian = ENDIAN.LittleEndian)
+        {
+            var ret = BitConverter.GetBytes(value);
+            var isToLittleEndian = (ENDIAN.LittleEndian == toEndian);
+            return (isToLittleEndian == BitConverter.IsLittleEndian) ? ret : ret.Reverse();
+        }
+    }
+}
---
+++ MyToolBoxCommonLibraryTests/ConvertersTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyToolBoxCommonLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MyToolBoxCommonLibrary.Tests
+{
+    [TestClass()]
+    public class ConvertersTests
+    {
+        [TestMethod()]
+        public void ToIeee754Test()
+        {
+            var bytes = Converters.ToIeee754(1.0f, ENDIAN.LittleEndian).ToArray();
+            Assert.AreEqual(0x00, bytes[0]);
+            Assert.AreEqual(0x00, bytes[1]);
+            Assert.AreEqual(0x80, bytes[2]);
+            Assert.AreEqual(0x3f, bytes[3]);
+
+            bytes = Converters.ToIeee754(1.0f, ENDIAN.BigEndian).ToArray();
+            Assert.AreEqual(0x3f, bytes[0]);
+            Assert.AreEqual(0x80, bytes[1]);
+            Assert.AreEqual(0x00, bytes[2]);
+            Assert.AreEqual(0x00, bytes[3]);
+
+        }
+    }
+}
