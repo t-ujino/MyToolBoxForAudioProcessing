8a59ed4ab941820d3e1b7b2aa472f4e4526161d9
Tadashi Ujino <t.ujino.priv+github@gmail.com>
2020-12-28 16:39:45 +0900

    :zap: TextBoxへの入力数値をそれぞれの形式で変換して表示する機能を追加

---
+++ MyToolBoxForAudioProcessing/ListViewDataContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MyToolBoxForAudioProcessing
+{
+    public class ListViewDataContext : INotifyPropertyChanged
+    {
+        private string name;
+        private string _value;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value == name) return;
+                name = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value) return;
+                _value = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- MyToolBoxForAudioProcessing/MainWindow.xaml.cs
+++ MyToolBoxForAudioProcessing/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,68 @@ namespace MyToolBoxForAudioProcessing
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<ListViewDataContext> listContext;
+        private Action<string> converter;
         public MainWindow()
         {
+            converter = ConvertFromReal;
+            listContext = new ObservableCollection<ListViewDataContext>();
+            listContext.Add(new ListViewDataContext() { Name = "REAL", Value = "0" });
+            listContext.Add(new ListViewDataContext() { Name = "dB", Value = "0" });
+            listContext.Add(new ListViewDataContext() { Name = "IEEE754", Value = "0" });
             InitializeComponent();
+            ListView1.ItemsSource = listContext;
+            ListView1.SelectedIndex = 0;
+            ListView1.SelectionChanged += (s, e) =>
+            {
+                if(ListView1.SelectedIndex == 0)
+                {
+                    converter = ConvertFromReal;
+                }else if(ListView1.SelectedIndex == 1)
+                {
+                    converter = ConvertFromDB;
+                }else if(ListView1.SelectedIndex == 2)
+                {
+                    converter = ConvertFromIeee754;
+                }
+            };
+        }
+
+        private void InputText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            converter?.Invoke(InputText.Text.Trim());
+        }
+        private void ConvertFromReal(float val)
+        {
+            listContext[0].Value = val.ToString("E8");
+            listContext[1].Name = (val >= 0) ? "dB [Pos]" : "dB [Neg]";
+            listContext[1].Value = (val == 0) ? "-inf" : (20 * Math.Log10(Math.Abs(val))).ToString("E8");
+            listContext[2].Value = "0x" + BitConverter.SingleToInt32Bits(val).ToString("X8");
+        }
+        private void ConvertFromReal(string input)
+        {
+            if(float.TryParse(input, out var val))
+            {
+                ConvertFromReal(val);
+            }
+        }
+
+        private void ConvertFromDB(string input)
+        {
+            if(float.TryParse(input, out var val))
+            {
+                var real = Math.Pow(10, val / 20);
+                ConvertFromReal((float)real);
+            }
+        }
+
+        private void ConvertFromIeee754(string input)
+        {
+            if(int.TryParse(input, System.Globalization.NumberStyles.HexNumber, null, out var val))
+            {
+                var real = BitConverter.Int32BitsToSingle(val);
+                ConvertFromReal(real);
+            }
         }
     }
 }
